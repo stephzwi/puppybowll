80ef1233c976ad9031754ace2389180d7327370e
mmmparlan <mmparlan@gmail.com>
2023-06-03 16:47:48 -0400

    coded cohartName, fetchAllPlayers, and fetchSinglePlayer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2302-ACC-ET-WEB-PT-E';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
@@ -12,7 +12,10 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  */
 const fetchAllPlayers = async () => {
     try {
-
+        const response = await fetch(`${APIURL}players`);
+        const result = await response.json();
+        console.log(result);
+        return result;
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
@@ -20,7 +23,9 @@ const fetchAllPlayers = async () => {
 
 const fetchSinglePlayer = async (playerId) => {
     try {
-
+        const response = await fetch(`${APIURL}players/${playerId}`);
+        const result = await response.json();
+        console.log(result);
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
